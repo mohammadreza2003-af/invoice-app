81b13a345ee32d931144e184681f8437a00ac301
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2024-07-13 23:41:31 +0430

    fixed dark mode and light mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,18 @@ import { Button } from "./ui/button";
 import { signout } from "@/services/auth";
 import { showToast } from "@/utils/helper";
 import { useQueryClient } from "@tanstack/react-query";
+import { Toggle } from "@/components/ui/toggle";
 import useAuth from "@/hooks/useAuth";
+import { Bold } from "lucide-react";
+import { useState } from "react";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
   const query = useQueryClient();
   const router = useRouter();
+  const { setTheme } = useTheme();
+
+  const [toggle, setToggle] = useState("system");
 
   const { isLogin } = useAuth();
   const handleSignout = async () => {
@@ -22,6 +29,15 @@ const Navbar = () => {
       });
       showToast("Successfuly", "Logout successful. See you next time!");
       router.push("/login");
+    }
+  };
+
+  const handleToggleChange = (isToggled: string) => {
+    console.log(isToggled);
+    if (isToggled) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
     }
   };
 
@@ -37,11 +53,28 @@ const Navbar = () => {
           router.push("/");
         }}
       />
-      <div className="flex gap-x-8 justify-between h-full items-center mr-4">
-        <ModeToggle />
+      <div className="flex sm:gap-x-8 gap-x-4 justify-between h-full items-center mr-4">
+        <div className="flex items-center justify-center">
+          <Button variant="none" onClick={() => setTheme("light")}>
+            <Image
+              src="/assets/icon-sun.svg"
+              width={24}
+              height={24}
+              alt="moon"
+            />
+          </Button>
+          <Button variant="none" onClick={() => setTheme("dark")}>
+            <Image
+              src="/assets/icon-moon.svg"
+              width={24}
+              height={24}
+              alt="moon"
+            />
+          </Button>
+        </div>
         <div className="w-[1px] h-full bg-slate-500 cursor-pointer" />
         <Image
-          className="rounded-full"
+          className="rounded-full sm:w-[40px] w-[32px]"
           src="/assets/image-avatar.jpg"
           width={40}
           height={40}
